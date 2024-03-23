07e70dde060a2917d8b7756a74748495e10f931b
Drew Walton <drewwalton19216801@gmail.com>
2024-03-23 10:36:17 -0600

    Refactor routing

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Medtracker\MedicationsController;
 use App\Http\Controllers\Medtracker\PharmacyController;
 use App\Http\Controllers\Medtracker\PrescriberController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Dashboard\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,40 +14,50 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
-
-Route::controller(MedtrackerController::class)->group(function () {
-    Route::get('/medtracker', 'index')->name('medtracker.index');
-    Route::get('/medtracker/reminders', 'reminders')->name('medtracker.reminders.index');
+Route::prefix('dashboard')->controller(DashboardController::class)->group(function () {
+    Route::get('/', 'index')->name('dashboard.index');
+    Route::get('/logout', 'logout')->name('dashboard.logout');
 });
 
-Route::controller(MedicationsController::class)->group(function () {
-    Route::get('/medtracker/medications', 'index')->name('medtracker.medications.index');
-    Route::get('/medtracker/medications/create', 'create')->name('medtracker.medications.create');
-    Route::post('/medtracker/medications/save', 'store')->name('medtracker.medications.store');
-    Route::post('/medtracker/medications/update/{id}', 'update')->name('medtracker.medications.update');
-    Route::get('/medtracker/medications/saveaspdf', 'savePdf')->name('medtracker.medications.pdf');
-    Route::get('/medtracker/medications/{id}', 'medication')->name('medtracker.medications.show');
-    Route::get('/medtracker/medications/{id}/edit', 'editMedication')->name('medtracker.medications.edit');
-    Route::get('/medtracker/medications/{id}/delete', 'destroy')->name('medtracker.medications.destroy');
+Route::prefix('dashboard/profile')->controller(ProfileController::class)->group(function () {
+    Route::get('/', 'profile')->name('dashboard.profile');
+    Route::post('/update', 'updateProfile')->name('dashboard.profile.update');
+    Route::post('/change-password', 'changePassword')->name('dashboard.profile.change-password');
 });
 
-Route::controller(PharmacyController::class)->group(function () {
-    Route::get('/medtracker/pharmacies', 'index')->name('medtracker.pharmacies.index');
-    Route::get('/medtracker/pharmacies/create', 'create')->name('medtracker.pharmacies.create');
-    Route::post('/medtracker/pharmacies/store', 'store')->name('medtracker.pharmacies.store');
-    Route::post('/medtracker/pharmacies/update/{id}', 'update')->name('medtracker.pharmacies.update');
-    Route::get('/medtracker/pharmacies/{id}', 'pharmacy')->name('medtracker.pharmacies.show');
-    Route::get('/medtracker/pharmacies/{id}/edit', 'editPharmacy')->name('medtracker.pharmacies.edit');
-    Route::get('/medtracker/pharmacies/{id}/destroy', 'destroy')->name('medtracker.pharmacies.destroy');
+Route::prefix('medtracker')->controller(MedtrackerController::class)->group(function () {
+    Route::get('/', 'index')->name('medtracker.index');
+    Route::get('/reminders', 'reminders')->name('medtracker.reminders.index');
 });
 
-Route::controller(PrescriberController::class)->group(function () {
-    Route::get('/medtracker/prescribers', 'index')->name('medtracker.prescribers.index');
-    Route::get('/medtracker/prescribers/create', 'create')->name('medtracker.prescribers.create');
-    Route::post('/medtracker/prescribers/store', 'store')->name('medtracker.prescribers.store');
-    Route::post('/medtracker/prescribers/update/{id}', 'update')->name('medtracker.prescribers.update');
-    Route::get('/medtracker/prescribers/{id}', 'prescriber')->name('medtracker.prescribers.show');
-    Route::get('/medtracker/prescribers/{id}/edit', 'editPrescriber')->name('medtracker.prescribers.edit');
-    Route::get('/medtracker/prescribers/{id}/destroy', 'destroy')->name('medtracker.prescribers.destroy');
+Route::prefix('medtracker/medications')->controller(MedicationsController::class)->group(function () {
+    Route::get('/', 'index')->name('medtracker.medications.index');
+    Route::get('/create', 'create')->name('medtracker.medications.create');
+    Route::post('/store', 'store')->name('medtracker.medications.store');
+    Route::post('/{id}/update', 'update')->name('medtracker.medications.update');
+    Route::get('/saveaspdf', 'savePdf')->name('medtracker.medications.pdf');
+    Route::get('/{id}', 'medication')->name('medtracker.medications.show');
+    Route::get('/{id}/edit', 'editMedication')->name('medtracker.medications.edit');
+    Route::get('/{id}/delete', 'destroy')->name('medtracker.medications.destroy');
 });
+
+Route::prefix('medtracker/pharmacies')->controller(PharmacyController::class)->group(function () {
+    Route::get('/', 'index')->name('medtracker.pharmacies.index');
+    Route::get('/create', 'create')->name('medtracker.pharmacies.create');
+    Route::post('/store', 'store')->name('medtracker.pharmacies.store');
+    Route::post('/{id}/update', 'update')->name('medtracker.pharmacies.update');
+    Route::get('/{id}', 'pharmacy')->name('medtracker.pharmacies.show');
+    Route::get('/{id}/edit', 'editPharmacy')->name('medtracker.pharmacies.edit');
+    Route::get('/{id}/delete', 'destroy')->name('medtracker.pharmacies.destroy');
+});
+
+Route::prefix('medtracker/prescribers')->controller(PrescriberController::class)->group(function () {
+    Route::get('/', 'index')->name('medtracker.prescribers.index');
+    Route::get('/create', 'create')->name('medtracker.prescribers.create');
+    Route::post('/store', 'store')->name('medtracker.prescribers.store');
+    Route::post('/{id}/update', 'update')->name('medtracker.prescribers.update');
+    Route::get('/{id}', 'prescriber')->name('medtracker.prescribers.show');
+    Route::get('/{id}/edit', 'editPrescriber')->name('medtracker.prescribers.edit');
+    Route::get('/{id}/delete', 'destroy')->name('medtracker.prescribers.destroy');
+});
+
