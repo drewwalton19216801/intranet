a2805170217c797570092463c53970069d40d1d7
Drew Walton <drewwalton19216801@gmail.com>
2024-03-22 22:43:10 -0600

    Actually show something on the dashboard

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -3,7 +3,51 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-12">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">{{ __('Server Information') }}</div>
+
+                <div class="card-body">
+                    <p class="card-text">
+                        <strong>Laravel version:</strong> {{ app()->version() }}
+                    </p>
+                    <p class="card-text">
+                        <strong>Server software:</strong> {{ $_SERVER['SERVER_SOFTWARE'] }}
+                    </p>
+                    <p class="card-text">
+                        <strong>IP address:</strong> {{ file_get_contents("https://ipecho.net/plain") }}
+                    </p>
+                    @if(strpos(PHP_OS, 'WIN') === false)
+                    <p class="card-text">
+                        <strong>Server uptime:</strong> {{ exec('uptime -p') }}
+                    </p>
+                    @endif
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+
+                    Your role is:
+                    @hasrole('admin')
+                        Admin
+                    @else
+                        User
+                    @endhasrole
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
