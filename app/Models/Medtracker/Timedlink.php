74a2b8ebe628e36699f363d572d6a3dde9e8375c
Drew Walton <drewwalton19216801@gmail.com>
2024-03-23 15:52:36 -0600

    Links models and migrations

---
+++ app/Models/Medtracker/Timedlink.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Medtracker;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Timedlink extends Model
+{
+    use HasFactory;
+}
